1dd604d53dd8ddbbb4d6280e11986d8a6d3600cb
Ajmal Jalal <ajmal@zennify.com>
2019-04-13 14:09:18 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="facecreater">
+        here we go
+
+      </div>
+    );
+  }
+}
+
+export default App;
